74028ed083b224f5d984386596c18c556284cd77
minatel <pedro.minatel@polygon.pt>
2020-05-05 22:00:42 +0100

    [FEATURE] Change on settings for serial port and baudrate

--- py_serial.py
+++ py_serial.py
@@ -3,17 +3,20 @@
 from datetime import datetime
 import serial
 import time
+import sys
+
+#Mudar port de acordo com a porta serial do gateway
+COMport = '/dev/ttyUSB0'
+COMspeed = 115200
 
 filename = datetime.now().strftime('datalog_%Y%m%d_%H%M%S')
 serial_log = open(filename + '.csv','a')
 
-#Mudar port de acordo com a porta serial do gateway
 #serial_ob = serial.Serial(port='COM5', baudrate=115200)
-serial_ob = serial.Serial(port="/dev/ttyUSB0",baudrate=115200)
+serial_ob = serial.Serial(port=COMport,baudrate=COMspeed)
 
 time.sleep( 1 )
 serial_ob.write(b'AT+START\r')
-time.sleep( 1 )
 
 try:
     while True:
