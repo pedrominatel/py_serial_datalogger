8b02e8a512060a044b25bad7033bb35eb5a73e47
minatel <pedro.minatel@polygon.pt>
2020-05-05 21:47:28 +0100

    [FEATURE] Added stop command and END tag on the CSV file

--- py_serial.py
+++ py_serial.py
@@ -2,16 +2,29 @@
 
 from datetime import datetime
 import serial
+import time
 
+filename = datetime.now().strftime('datalog_%Y%m%d_%H%M%S')
+serial_log = open(filename + '.csv','a')
+
+#Mudar port de acordo com a porta serial do gateway
+#serial_ob = serial.Serial(port='COM5', baudrate=115200)
 serial_ob = serial.Serial(port="/dev/ttyUSB0",baudrate=115200)
-filename = datetime.now().strftime("datalog_%Y%m%d_%H%M%S")
+
+time.sleep( 1 )
+serial_ob.write(b'AT+START\r')
+time.sleep( 1 )
 
 try:
     while True:
-        serial_log = open(filename + '.csv','a')
-        serial_log.write(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f,"))
-
+        
+        serial_log.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f,'))
                 +str(serial_ob.readline()))
-        serial_log.close()
+        
 except KeyboardInterrupt:
+    print('Finalizando a coleta dos dados...')
+    serial_ob.write(b'AT+STOP\r')
+    serial_ob.close()
+    serial_log.write('### FIM DA COLETA ###')
+    serial_log.close()
     pass
