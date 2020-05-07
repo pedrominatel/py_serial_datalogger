6d5c2f067f931e8f7c37df3140121fe79736a799
Douglas Esteves <douglas.brsoftware@gmail.com>
2020-05-07 10:05:55 -0300

    Update py_serial.py

--- py_serial.py
+++ py_serial.py
@@ -5,9 +5,8 @@ import serial
 import time
 import keyboard
 
-
 #Mudar port de acordo com a porta serial do gateway
-COMport = '/dev/ttyUSB0'
+COMport = 'COM3'
 COMspeed = 115200
 
 filename = datetime.now().strftime('datalog_%Y%m%d_%H%M%S')
@@ -19,10 +18,19 @@ serial_ob = serial.Serial(port=COMport,baudrate=COMspeed)
 time.sleep( 1 )
 serial_ob.write(b'AT+START\r')
 
-while True:  # making a loop
+while True:  # making a loop      
     try:  # used try so that if user pressed other than the given key error will not be shown
+
+        serial_log.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f,'))
+                +str(serial_ob.readline()))
+
         if keyboard.is_pressed("q"):  # if key 'q' is pressed 
             print('You Pressed A Key!')
+            print('Finalizando a coleta dos dados...')
+            serial_ob.write(b'AT+STOP\r')
+            serial_ob.close()
+            serial_log.write('### FIM DA COLETA ###')
+            serial_log.close()            
             break  # finishing the loop
     except:
         print('Finalizando a coleta dos dados...')
@@ -31,9 +39,6 @@ while True:  # making a loop
 try:
     while True:
         
-        serial_log.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f,'))
-                +str(serial_ob.readline()))
-
         try:  # used try so that if user pressed other than the given key error will not be shown
             if keyboard.is_pressed("q"):  # if key 'q' is pressed 
                 print('Finalizando a coleta dos dados...')
@@ -52,6 +57,6 @@ except KeyboardInterrupt:
     serial_log.write('### FIM DA COLETA ###')
     serial_log.close()
 except:
-    print "?"
+    print ("?")
 finally:
-    print "Closing...."
+    print ("Closing....")
